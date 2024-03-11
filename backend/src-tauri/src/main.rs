e5116fd2290a636fcc04a717685525f686ba900f
Zelkiron <140980003+Zelkiron@users.noreply.github.com>
2024-03-10 20:34:09 -0400

    changed some things

--- backend/src-tauri/src/main.rs
+++ backend/src-tauri/src/main.rs
@@ -1,20 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use std::process::Command;
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .invoke_handler(tauri::generate_handler![openFile])
+    .invoke_handler(tauri::generate_handler![open_file])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
-
 #[tauri::command]
-fn greet(name: &str) -> String {
-  format!("Hello, {}!", name)
-}
-
-#[tauri::command]
-fn openFile() {
-  Command::new("explorer").arg(".").spawn().unwrap();
+fn open_file() {
+  //Command::new("explorer").arg(".").spawn().unwrap();
+  let file = open::that(".");
 }
--- frontend/src/lib/Greet.svelte
+++ frontend/src/lib/Greet.svelte
@@ -1,21 +1,12 @@
 <script>
     import { invoke } from '@tauri-apps/api/tauri'
-  
-    let name = ''
-    let greetMsg = ''
-  
-    async function greet() {
-      greetMsg = await invoke('greet', { name })
-    }
 
-    async function openFile() {
-      await invoke('openFile')
+    async function open_file() {
+      await invoke('open_file')
     }
   </script>
   
   <div>
-    <input id="greet-input" placeholder="Enter a name..." bind:value="{name}" />
-    <button on:click="{greet}">Greet</button>
-    <button on:click="{openFile}">Open File</button>
-    <p>{greetMsg}</p>
+    <button on:click="{open_file}">Open File</button>
+    <h1>No Open File...
   </div>
